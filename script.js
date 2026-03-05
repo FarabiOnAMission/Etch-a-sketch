07e5fdc0ddcebf7519f4599f5073c08d900beca4
Farabi <86078777+FarabiOnAMission@users.noreply.github.com>
2026-03-05 19:32:19 +0600

    Added the hover effect on grids

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 w=4;
 
+const btn = document.querySelector("#btn");
+cont = document.querySelector(".container");
+
 function createDiv(){
     const sqr = document.createElement("div");
     sqr.style.width = `calc(100% / ${w})`;
@@ -9,9 +12,32 @@ function createDiv(){
     return sqr;
 }
 
+function createGrid(){
 for(let i=1;i<=w*w;i++){
-    cont = document.querySelector(".container");
     const stor = createDiv();
+    stor.classList.add('smolBox');
     cont.appendChild(stor);
 
 }
+}
+
+createGrid();
+
+btn.addEventListener('click',function(){
+    w = prompt("Enter a dimension of the row: ");
+    cont.innerHTML = "";
+    createGrid();
+});
+
+
+cont.addEventListener('mouseover',function(e){
+    if(e.target.classList.contains('smolBox')){
+        e.target.classList.add('hovered');
+    }
+});
+
+cont.addEventListener('mouseout',function(e){
+    if(e.target.classList.contains('smolBox')){
+        e.target.classList.remove('hovered');
+    }
+});
